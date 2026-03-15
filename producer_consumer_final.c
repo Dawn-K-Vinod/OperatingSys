3ae52bac337b9fa32209e02fb898f41edc858391
Dawn K Vinod <183817851+Dawn-K-Vinod@users.noreply.github.com>
2026-03-15 23:21:27 +0530

    Update producer_consumer_final.c

--- producer_consumer_final.c
+++ producer_consumer_final.c
@@ -1,46 +1,50 @@
 /*
- A *uthor     : Dawn K Vinod
- Description: A simple C program (even though we can do it better) to show the simulation of Producer-Consumer solution.
- This has to be done using the Semaphores "empty", "full", "mutex".
- But since we are using 2 basic functions `produce()` and `consume()` and only one function runs at a time, the "mutex" is not needed.
- */
+Author     : Dawn K Vinod
+Description: A simple C program (even though we can do it better) to show the simulation of Producer-Consumer solution.
+             This has to be done using the Semaphores "empty", "full", "mutex".
+             But since we are using 2 basic functions `produce()` and `consume()` and only one function runs at a time, the "mutex" is not needed.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define BUFFER_SIZE 7
+#define BUFFER_SIZE 5
 
 int buffer[BUFFER_SIZE];
 int head = 0, tail = 0; // To mark the FRONT and REAR of the Circular queue `buffer[]`
 int empty = BUFFER_SIZE, full = 0;
 int item = 0; int count = 0;
 
-void produce() {
-    if (full == BUFFER_SIZE)
-        return; // Buffer is full, cannot produce
-        buffer[tail % BUFFER_SIZE] = item;
-    tail = (tail + 1) % BUFFER_SIZE;
-    full++; empty--;
-    count++;
-    printf("Producer produces: %d\n", item);
-}
-
-void consume() {
-    if (empty == BUFFER_SIZE)
-        return; // Buffer is empty, cannot consume
-        printf("Consumer consumes: %d\n", buffer[head % BUFFER_SIZE]);
-    head = (head + 1) % BUFFER_SIZE;
-    empty++; full--;
-    count--;
-}
 // To print the BUFFER contents
 void printBuffer() {
-    printf("Buffer contents: [");
+    printf("\nBuffer contents: [");
     for (int i = 0; i < count; i++) {
         int index = (head + i) % BUFFER_SIZE;
         printf("%d ", buffer[index]);
     }
-    printf("]\n\n");
+    printf("]\n");
+}
+
+void produce() {
+    if (full == BUFFER_SIZE)
+        {printf("Waiting for consumer to consume...\n"); return;} // Buffer is full, cannot produce
+    buffer[tail % BUFFER_SIZE] = item;
+    tail = (tail + 1) % BUFFER_SIZE;
+    full++; empty--;
+    count++;
+    printf("\nProducer produces: %d", item);
+    printBuffer();
+}
+
+void consume() {
+    if (empty == BUFFER_SIZE)
+        {printf("Waiting for producer to produce...\n"); return;} // Buffer is empty, cannot consume
+    printf("\nConsumer consumes: %d", buffer[head % BUFFER_SIZE]);
+    head = (head + 1) % BUFFER_SIZE;
+    empty++; full--;
+    count--;
+    printBuffer();
 }
 
 int main() {
@@ -51,14 +55,13 @@ int main() {
     scanf("%d", &NUM_OPERATIONS);
 
     int k = NUM_OPERATIONS;
+    srand(time(NULL));
     while (k > 0) {
-        random_num = rand() % 11;
         item = 100 + (rand() % 101);
-        if (random_num % 2 == 0)
+        if (item % 2 == 0) // same variable `item` is used for randomness, since the time is already a random number
             produce();
         else
             consume();
-        printBuffer();
         k--;
     }
     return 0;
